ee11250bf2d965c425b7fb9ea42afef359dad2ea
Edmound Njoori <edmound776@gmail.com>
2025-06-03 20:20:44 +0300

    Add the admin routes and layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,9 @@ import Error404 from "./pages/Error404";
 import ProtectedRoute from "@features/auth/ProtectedRoute";
 import LoginPage from "@features/auth/LoginPage";
 import RegisterPage from "@features/auth/RegisterPage";
+import Adminlayout from "./components/page_layouts/admin_layout";
+import AdminHome from "@features/admin/pages/Homepage";
+import { SidebarProvider } from "./components/ui/sidebar";
 
 
 function App() {
@@ -22,13 +25,21 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<LandingPage />} />
-            <Route element={<ProtectedRoute roles={["admin","client"]} />} >
+            <Route element={<ProtectedRoute roles={["admin", "client"]} />} >
 
-            <Route path="home" element={<HomePage />} />
+              <Route path="home" element={<HomePage />} />
             </Route>
             <Route path="auth/register" element={<RegisterPage />} />
             <Route path="auth/login" element={<LoginPage />} />
             <Route path="*" element={<Error404 />} />
+          </Route>
+          <Route path="/admin" element={
+            <SidebarProvider>
+              <Adminlayout />
+            </SidebarProvider>
+          }
+          >
+            <Route index element={<AdminHome />} />
           </Route>
         </Routes>
       </Router>
