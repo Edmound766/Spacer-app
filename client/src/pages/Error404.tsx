7a61b5a020390ac8d2464a737aac1fe42bde0c8e
Edmound Njoori <edmound776@gmail.com>
2025-06-03 20:19:04 +0300

    Update the page not found page

--- client/src/pages/Error404.tsx
+++ client/src/pages/Error404.tsx
@@ -1,20 +1,37 @@
 import { Button } from "@/components/ui/button";
-import { Link, useLocation } from "react-router";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { TriangleAlert } from "lucide-react";
+import { NavLink, useLocation } from "react-router";
 
 export default function Error404() {
-    
-    const location = useLocation()
-    console.log(location);
-    
-     const from = location.state?.from?.pathname || "/";
-    return (
-        <div className="flex flex-col justify-around">
-            <h1>Error 404</h1>
-            <p>Page not found </p>
-            <Button variant={"secondary"}>
 
-            <Link to={from} replace> Return</Link>
-            </Button>
-        </div>
-    )
+  const location = useLocation()
+  console.log(location);
+
+  const from = location.state?.from?.pathname || "/home";
+  return (
+    <div className="flex h-full justify-center pt-10">
+      <Card className="w-2/4 h-2/4">
+        <CardHeader>
+          <CardTitle>Page not found</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col items-center">
+
+            <TriangleAlert size={80} color="red" />
+            <h1 className="text-red-500">Error 404</h1>
+          </div>
+        </CardContent>
+
+        <CardFooter>
+
+          <Button className="align-middle">
+            <NavLink to={from}>
+              Return</NavLink>
+          </Button>
+
+        </CardFooter>
+      </Card>
+    </div>
+  )
 }
